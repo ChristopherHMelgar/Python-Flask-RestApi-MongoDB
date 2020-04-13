514c239ed96aa97e50e1c92aaf312202d5fdc9e5
ChristopherHMelgar <christopherhmelgar@gmail.com>
2020-04-12 21:48:09 -0300

    init flask api

---
+++ README.md
@@ -0,0 +1,19 @@
+# Simple Python Flask Api Rest
+
+## Setup
+
+* Start virtual environment 
+
+``` source venv/bin/activate ```
+
+* Install packages
+
+``` pip install -r requirements.txt ```
+
+>If you need generate a requirements.txt just use this command:
+
+````pip freeze > requirements.txt ``` 
+
+* Run the application 
+
+``` flask run ```
---
+++ app/routes.py
@@ -0,0 +1,81 @@
+from flask import Flask, request, jsonify, Response
+from app import app, mongo
+from werkzeug.security import generate_password_hash, check_password_hash
+from bson import json_util
+from bson.objectid import ObjectId
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "User Api"
+
+@app.route('/users', methods=['POST'])
+def create_user():
+   # Receiving data
+   username = request.json['username']
+   password = request.json['password']
+   email = request.json['email']
+
+   if username and password and email:
+      hashed_password = generate_password_hash(password)
+      id = mongo.db.users.insert(
+         {'username': username, 'password': hashed_password, 'email': email}
+      )
+      response = {
+         'id': str(id),
+         'username': username,
+         'password': hashed_password,
+         'email': email
+      }
+      return response
+   else: 
+      return not_found()
+
+   return {'message': 'received'}
+
+@app.route('/users', methods=['GET'])
+def get_users():
+   users = mongo.db.users.find()
+   response = json_util.dumps(users)
+   return Response(response, mimetype='application/json')
+
+@app.route('/users/<id>', methods=['GET'])
+def get_user(id):
+   user = mongo.db.users.find_one({'_id': ObjectId(id)})
+   response = json_util.dumps(user)
+   return Response(response, mimetype='application/json')
+
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+   mongo.db.users.delete_one({'_id': ObjectId(id)})
+   response = jsonify({
+      'message': 'User ' + id + ' was delete successfull'
+   })
+   return response
+
+@app.route('/users/<id>', methods=['PUT'])
+def update_user(id):
+   username = request.json['username']
+   password = request.json['password']
+   email = request.json['email']
+
+   if username and password and email:
+      hashed_password = generate_password_hash(password)
+      mongo.db.users.update_one({'_id': ObjectId(id)}, {'$set': {
+         'username': username,
+         'password': hashed_password,
+         'email': email 
+      }})
+      response = jsonify({
+         'message': 'User ' + id + ' was updated successfull'
+      })
+      return response
+
+@app.errorhandler(404)
+def not_found(error=None):
+   response = jsonify({
+      'message': 'Resource Not Found: ' + request.url,
+      'status': 404 
+   })
+   response.status_code = 404
+   return response
